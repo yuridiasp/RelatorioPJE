b225f5037a8549a11d58675a0bda00c854784817
Yuri Dias Pereira <yuristardias@hotmail.com>
2022-08-04 15:03:47 -0300

    Atualização
    Implementado requisição dinâmica para todos os sistemas PJe;
    Codificado formatação do relatório PDF;
    Implementado quebra automática de página;
    Removido console.log's;
    Removido constantes/variáveis desnecessárias.

--- content.js
+++ content.js
@@ -3,14 +3,13 @@ let inputDateFinal
 let InputCurrentDate
 let nome = []
 let viewState
-const pje2x1g = 'https://pje1g.trf5.jus.br/pje/Painel/painel_usuario/advogado.seam'
-const input = document.querySelector("#_viewRoot\\:status\\.start")
+const loader = document.querySelector("#_viewRoot\\:status\\.start")
 const state = {
     active: null
 }
 
 async function ajax(payload) {
-    console.log(payload)
+
     return new Promise(
         (resolve, reject) => {
             // uso da API para fornecer acesso a realizar requests ao servidor
@@ -25,7 +24,6 @@ async function ajax(payload) {
                 // preenchimento do resultado no HTML
                 let parser = new DOMParser()
                 let doc = parser.parseFromString(response,'text/html')
-                console.log(doc)
                 resolve(doc)
                 }
 
@@ -35,7 +33,7 @@ async function ajax(payload) {
                 }
             };
             // URL
-            let url = '/pje/Painel/painel_usuario/advogado.seam'
+            let url = window.location.href
 
             // configuração para request
             xhttp.open("POST", url, true),
@@ -49,7 +47,7 @@ async function ajax(payload) {
 
 async function requestSend(page) {
     try {
-        const payload = `AJAXREQUEST=conteudoAbaExpedientes&formExpedientes%3AnumeroProcesso%3AnumeroSequencial=&formExpedientes%3AnumeroProcesso%3AnumeroDigitoVerificador=&formExpedientes%3AnumeroProcesso%3AAno=&formExpedientes%3AnumeroProcesso%3AramoJustica=&formExpedientes%3AnumeroProcesso%3ArespectivoTribunal=&formExpedientes%3AnumeroProcesso%3ANumeroOrgaoJustica=&formExpedientes%3AitNE=&formExpedientes%3AitCL=&formExpedientes%3AitAS=&formExpedientes%3Aj_id455InputDate=${inputDateInicial}&formExpedientes%3Aj_id455InputCurrentDate=${InputCurrentDate}&formExpedientes%3Aj_id457InputDate=${inputDateFinal}&formExpedientes%3Aj_id457InputCurrentDate=${InputCurrentDate}&formExpedientes%3AitDestPend=&formExpedientes%3AitIMF=&formExpedientes%3AitOAB=&formExpedientes%3AsoOrd=org.jboss.seam.ui.NoSelectionConverter.noSelectionValue&formExpedientes%3AitPR=org.jboss.seam.ui.NoSelectionConverter.noSelectionValue&${nome[0]}=${nome[0]}&autoScroll=&${nome[1]}=&${nome[2]}=&javax.faces.ViewState=${viewState}&javax.faces.ViewState=${viewState}&formExpedientes=formExpedientes&formExpedientes%3AtbExpedientes%3AscPendentes=${page}&ajaxSingle=formExpedientes%3AtbExpedientes%3AscPendentes&AJAX%3AEVENTS_COUNT=1&`
+        const payload = `AJAXREQUEST=conteudoAbaExpedientes&formExpedientes%3AnumeroProcesso%3AnumeroSequencial=&formExpedientes%3AnumeroProcesso%3AnumeroDigitoVerificador=&formExpedientes%3AnumeroProcesso%3AAno=&formExpedientes%3AnumeroProcesso%3AramoJustica=&formExpedientes%3AnumeroProcesso%3ArespectivoTribunal=&formExpedientes%3AnumeroProcesso%3ANumeroOrgaoJustica=&formExpedientes%3AitNE=&formExpedientes%3AitCL=&formExpedientes%3AitAS=&formExpedientes%3Aj_id455InputDate=${inputDateInicial}&formExpedientes%3Aj_id455InputCurrentDate=${InputCurrentDate}&formExpedientes%3Aj_id457InputDate=${inputDateFinal}&formExpedientes%3Aj_id457InputCurrentDate=${InputCurrentDate}&formExpedientes%3AitDestPend=&formExpedientes%3AitIMF=&formExpedientes%3AitOAB=&formExpedientes%3AsoOrd=org.jboss.seam.ui.NoSelectionConverter.noSelectionValue&formExpedientes%3AitPR=org.jboss.seam.ui.NoSelectionConverter.noSelectionValue&${nome[0].name.replaceAll(':','%3A')}=${nome[0].name.replaceAll(':','%3A')}&autoScroll=&${nome[1].name.replaceAll(':','%3A')}=&${nome[2].name.replaceAll(':','%3A')}=&javax.faces.ViewState=${viewState}&javax.faces.ViewState=${viewState}&formExpedientes=formExpedientes&formExpedientes%3AtbExpedientes%3AscPendentes=${page}&ajaxSingle=formExpedientes%3AtbExpedientes%3AscPendentes&AJAX%3AEVENTS_COUNT=1&`
 
         return await ajax(payload)
     }
@@ -70,60 +68,77 @@ const observer = new MutationObserver(function(mutations) {
 })
 
 function PDFGen (array) {
-    let column = [[],[]]
+    let content = [[],[]]
     let cont = 0
 
     for (let index = 0; index < array.length; index++) {
-        ++cont
-        if (cont <= 65)
-            column[0].push(array[index])
+        if (cont < 16) {
+            content[0].push(array[index])
+            cont++
+        }
         else
-            column[1].push(array[index])
-        if (cont == 65*2)
-            cont = 0
-    }
+            if (cont <= 32) {
+                content[1].push(array[index])
+                cont++
+            }
+            else
+                cont = 1
+    }                        
+
 
     var docDefinition = {
         content: [
             {
-            columns: [
-                {
-                // auto-sized columns have their widths based on their content
-                width: '50%',
-                text: column[0]
-                },
-                {
-                // star-sized columns fill the remaining space
-                // if there's more than one star-column, available width is divided equally
-                width: '50%',
-                text: column[1]
-                }
-            ],
-            // optional space between columns
-            columnGap: 10
-            },
+                columns: [
+                    {
+                        width: '50%',
+                        stack: content[0]
+                    },
+                    {
+                        width: '50%',
+                        stack: content[1]
+                    }
+                ] 
+            }
         ],
-        pageBreakBefore: function(currentNode, followingNodesOnPage, nodesOnNextPage, previousNodesOnPage) {
-            return currentNode.headlineLevel === 1 && followingNodesOnPage.length === 0;
-         }
-        }
-            
+    }
+
     pdfMake.createPdf(docDefinition).open()
 }
 
+
 function createInputDateAndInsertDates() {
+    if (document.querySelector('#formExpedientes\\:tbExpedientes\\:tb').children[0].children[3] == undefined) {
+        nome.push(document.querySelector('#formExpedientes\\:tbExpedientes\\:tb').children[0].children[0].children[0].children[0].children[4].children[0])
+    }
+    else {
+        nome.push(document.querySelector('#formExpedientes\\:tbExpedientes\\:tb').children[0].children[3].children[1].children[2])
+    }
+
+    if (document.querySelector('#formExpedientes\\:tbExpedientes\\:tb').children[0].children[3] == undefined) {
+        nome.push(document.querySelector('#formExpedientes\\:tbExpedientes\\:tb').children[0].children[0].children[0].children[0].children[4].children[4])
+    }
+    else {
+        nome.push(document.querySelector('#formExpedientes\\:tbExpedientes\\:tb').children[0].children[3].children[1].children[4])
+    }
+
+    if (document.querySelector('#formExpedientes\\:tbExpedientes\\:tb').children[0].children[3] == undefined) {
+        nome.push(document.querySelector('#formExpedientes\\:tbExpedientes\\:tb').children[0].children[0].children[0].children[0].children[4].children[5])
+    }
+    else {
+        nome.push(document.querySelector('#formExpedientes\\:tbExpedientes\\:tb').children[0].children[3].children[1].children[5])
+    }
+    
     viewState = document.querySelector("#javax\\.faces\\.ViewState").value
-    inputDateInicial = document.querySelector("#formExpedientes\\:j_id455InputDate").value.replaceAll('/','%2F')
-    inputDateFinal = document.querySelector("#formExpedientes\\:j_id457InputDate").value.replaceAll('/','%2F')
-    InputCurrentDate = document.querySelector("#formExpedientes\\:j_id457InputCurrentDate").value.replaceAll('/','%2F')
-    nome.push(document.querySelector('#formExpedientes\\:tbExpedientes\\:tb').children[0].children[3].children[1].children[2].name.replaceAll(':','%3A'))
-    nome.push(document.querySelector('#formExpedientes\\:tbExpedientes\\:tb').children[0].children[3].children[1].children[4].name.replaceAll(':','%3A'))
-    nome.push(document.querySelector('#formExpedientes\\:tbExpedientes\\:tb').children[0].children[3].children[1].children[5].name.replaceAll(':','%3A'))
-    if (inputDateInicial.length == 0 && inputDateFinal.length == 0)
+    inputDateInicial = document.querySelector("#divCampos > div:nth-child(2) > div.form-group.col-xs-4.col-sm-4 > div").children[0].children[0]
+    inputDateFinal = document.querySelector("#divCampos > div:nth-child(2) > div.form-group.col-xs-4.col-sm-4 > div").children[4].children[0]
+    InputCurrentDateInicial = document.querySelector("#divCampos > div:nth-child(2) > div.form-group.col-xs-4.col-sm-4 > div").children[0].children[2]
+    InputCurrentDateFinal = document.querySelector("#divCampos > div:nth-child(2) > div.form-group.col-xs-4.col-sm-4 > div").children[4].children[2]
+
+    if (inputDateInicial.value.length == 0 && inputDateFinal.value.length == 0)
         return
     if (document.getElementById('gerarPDF') != null)
         return
-    const loader = document.querySelector("#_viewRoot\\:status\\.start")
     const title = document.querySelector("#formExpedientes\\:Filtros > div > div.col-xs-8.col-md-8")
     let button = document.createElement('input')
     
@@ -134,42 +149,29 @@ function createInputDateAndInsertDates() {
 
 
     button.addEventListener('click', async function formatIntimacoes () {
-        let intimacoes = []
         let array_intimacoes = []
+        let intimacoes = []
+        let params
         let tamanho
         let colunas
-        let params
-        let contador = 0
 
         const abas = document.querySelectorAll("#formExpedientes\\:tbExpedientes\\:scPendentes_table > tbody > tr > td")
-        loader.style.display = 'block'
+        if (loader != null)
+            loader.style.display = 'block'
         if (abas.length == 0) {
             params = document.querySelector("#formExpedientes\\:tbExpedientes\\:tb").children
             for (let index = 0; index < params.length; index++) 
                 intimacoes.push(params[index].children[1])
 
             intimacoes.forEach(e => {
-                ++contador
                 tamanho = intimacoes.length
                 colunas = e.children[0].children
-                array_intimacoes.push({text: colunas[0].children[0].innerText + '\n',fontSize: 10, bold:true})
-                array_intimacoes.push({text: colunas[0].children[1].innerText + '\n',fontSize: 10})
-                array_intimacoes.push({text: colunas[0].children[2].innerText + '\n',fontSize: 10})
-                array_intimacoes.push({text: colunas[0].children[3].innerText + '\n',fontSize: 10})
-                array_intimacoes.push({text: colunas[0].children[4].innerText + '\n',fontSize: 10})
-                array_intimacoes.push({text: colunas[0].children[7].innerText.substring(0,50) + '\n',fontSize: 10})
-                array_intimacoes.push({text: colunas[1].children[0].children[0].innerText.split(' ')[1] + '\n',fontSize: 10, bold: true, color: '#058EC4'})
+    
+                array_intimacoes.push({text: colunas[0].children[0].innerText + '\n' + colunas[0].children[1].innerText + '\n' + colunas[0].children[2].innerText + '\n' + colunas[0].children[3].innerText + '\n' + colunas[0].children[4].innerText + '\n' + colunas[0].children[7].innerText.substring(0,50) + '\n' + colunas[1].children[0].children[0].innerText.split(' ')[1] + '\n',fontSize: 10,unbreakable: true},{text: '\n',fontSize: 10})
+    
                 //array_intimacoes.push({text: colunas[1].children[0].children[1].innerText + '\n',fontSize: 8})
                 //array_intimacoes.push({text: colunas[1].children[0].children[2].innerText + '\n',fontSize: 10})
-                //array_intimacoes.push({text: colunas[1].children[0].children[3].innerText + '\n',fontSize: 10})
-                if (contador == 8) {
-                    array_intimacoes.push({text: '.\n',fontSize: 10})
-                    array_intimacoes.push({text: '.\n',fontSize: 10})
-                    contador = 0
-                }
-                else {
-                    array_intimacoes.push({text: '.\n',fontSize: 10})
-                }                
+                //array_intimacoes.push({text: colunas[1].children[0].children[3].innerText + '\n',fontSize: 10})          
             })
         }
         else {
@@ -183,31 +185,18 @@ function createInputDateAndInsertDates() {
                     tamanho = e.documentElement.querySelector("#formExpedientes\\:tbExpedientes\\:tb").children.length
                     for (let index = 0; index < tamanho; index++) {
                         colunas = e.documentElement.querySelector("#formExpedientes\\:tbExpedientes\\:tb").children[index].children[1].children[0].children
-                        array_intimacoes.push({text: colunas[0].children[0].innerText + '\n',fontSize: 10, bold:true})
-                        array_intimacoes.push({text: colunas[0].children[1].innerText + '\n',fontSize: 10})
-                        array_intimacoes.push({text: colunas[0].children[2].innerText + '\n',fontSize: 10})
-                        array_intimacoes.push({text: colunas[0].children[3].innerText + '\n',fontSize: 10})
-                        array_intimacoes.push({text: colunas[0].children[4].innerText + '\n',fontSize: 10})
-                        array_intimacoes.push({text: colunas[0].children[7].innerText.substring(0,50) + '\n',fontSize: 10})
-                        array_intimacoes.push({text: colunas[1].children[0].children[0].innerText.split(' ')[1] + '\n',fontSize: 10, bold: true, color: '#058EC4'})
+
+                        array_intimacoes.push({text: colunas[0].children[0].innerText + '\n' + colunas[0].children[1].innerText + '\n' + colunas[0].children[2].innerText + '\n' + colunas[0].children[3].innerText + '\n' + colunas[0].children[4].innerText + '\n' + colunas[0].children[7].innerText.substring(0,50) + '\n' + colunas[1].children[0].children[0].innerText.split(' ')[1] + '\n',fontSize: 10,unbreakable: true},{text: '\n',fontSize: 10})
                         //array_intimacoes.push({text: colunas[1].children[0].children[1].innerText + '\n',fontSize: 8})
                         //array_intimacoes.push({text: colunas[1].children[0].children[2].innerText + '\n',fontSize: 10})
                         //array_intimacoes.push({text: colunas[1].children[0].children[3].innerText + '\n',fontSize: 10})
-                        if (contador == 8) {
-                            array_intimacoes.push({text: '\n',fontSize: 10})
-                            array_intimacoes.push({text: '\n',fontSize: 10})
-                            array_intimacoes.push({text: '\n',fontSize: 10})
-                            contador = 0
-                        }
-                        else {
-                            array_intimacoes.push({text: '\n',fontSize: 10})
-                            ++contador
-                        }
                     }
                 })
             }
+
             PDFGen(array_intimacoes)
-            loader.style.display = 'none'
+            if (loader != null)
+                loader.style.display = 'none'
     })
 }
 
@@ -215,7 +204,7 @@ async function activate() {
     
     state.active = await getActive()
     if (state.active == "Desativado") {
-        observer.observe(input, {attributes: true})
+        observer.observe(loader, {attributes: true})
         return
     }
 }
@@ -248,4 +237,5 @@ function updateEvent() {
 (function () {
     updateEvent()
     activate()
+    //testeGen()
 })()
